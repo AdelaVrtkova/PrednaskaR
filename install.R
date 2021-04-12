3317b85e7b7718e788ce543ed6e1215d1eea36a3
AdelaVrtkova <adela.vrtkova@vsb.cz>
2021-04-12 14:23:49 +0200

    Prednaska_files

--- install.R
+++ install.R
@@ -1,8 +1,8 @@
 install.packages("readxl")
 install.packages("dplyr")
-install.packages("openxlsx")
 install.packages("moments")
 install.packages("ggplot2")
+install.packages("tidyr")
 
 install.packages('IRkernel')
 IRkernel::installspec(user = TRUE)
