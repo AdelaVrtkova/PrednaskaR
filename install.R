1850c996d9515e29cc13cc2d11a761aa30d6b8e6
AdelaVrtkova <adela.vrtkova@vsb.cz>
2021-04-12 19:27:33 +0200

    Update_files

--- install.R
+++ install.R
@@ -3,5 +3,10 @@ install.packages("dplyr")
 install.packages("openxlsx")
 install.packages("moments")
 install.packages("ggplot2")
+install.packages("devtools")
 
-install.packages("tidyverse")
+devtools::install_version(  
+  "tidyr",
+  version = "1.0.0",
+  repos = "http://cran.us.r-project.org"  
+)
