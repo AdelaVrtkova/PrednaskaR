510c9275b649cd6eb24be3b7493d76413c15b0a7
AdelaVrtkova <adela.vrtkova@vsb.cz>
2021-04-12 15:45:36 +0200

    Update_files

--- install.R
+++ install.R
@@ -3,11 +3,7 @@ install.packages("dplyr")
 install.packages("openxlsx")
 install.packages("moments")
 
-install.packages("lawstat")
-install.packages("BSDA")
-install.packages("EnvStats")
-install.packages("binom")
-
+install.packages("tidyr")
 
 install.packages('IRkernel')
 IRkernel::installspec(user = TRUE)
