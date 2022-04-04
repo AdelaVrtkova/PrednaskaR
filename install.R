921903797831db300cdf7f6dc0177c7b9760247d
AdelaVrtkova <adela.vrtkova@vsb.cz>
2022-04-04 10:38:47 +0200

    Lecture_files

---
+++ install.R
@@ -0,0 +1,12 @@
+install.packages("readxl")
+install.packages("dplyr")
+install.packages("openxlsx")
+install.packages("moments")
+install.packages("ggplot2")
+install.packages("devtools")
+
+devtools::install_version(  
+  "tidyr",
+  version = "1.0.0",
+  repos = "http://cran.us.r-project.org"  
+)
